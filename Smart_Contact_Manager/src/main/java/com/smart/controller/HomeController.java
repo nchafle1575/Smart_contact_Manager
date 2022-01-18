88e655e9b90ab2ec40456092601d9632a652517b
Nandkishor Chafle <92887229+nchafle1575@users.noreply.github.com>
2022-01-19 01:08:42 +0530

    Add files via upload
    First Commit, Registration / server-side validation are working...

---
+++ Smart_Contact_Manager/src/main/java/com/smart/controller/HomeController.java
@@ -0,0 +1,114 @@
+package com.smart.controller;
+
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.smart.dao.UserRepository;
+import com.smart.entities.User;
+import com.smart.helper.Message;
+
+
+@Controller
+public class HomeController {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@GetMapping("/")
+	public String home(Model m) {
+		m.addAttribute("title", "Home - Smart Contact Manager");
+		return "home";
+	}
+
+	@GetMapping("/about")
+	public String about(Model m) {
+		m.addAttribute("title", "about - Smart Contact Manager");
+		return "about";
+	}
+
+	@GetMapping("/signup")
+	public String signup(Model m) {
+		m.addAttribute("title", "signup - Smart Contact Manager");
+		m.addAttribute("user", new User());
+		return "signup";
+	}
+
+	
+//	@PostMapping("/process")
+//	public String processForm( @Valid @ModelAttribute("loginData") LoginData loginData, BindingResult result) {
+//		System.out.println(loginData);
+//		
+//		if(result.hasErrors()) {
+//			
+//			System.out.println(result);
+//			return "form";
+//		}
+//		
+//		//data process
+//		return "success";
+//	}
+	
+	
+	// this handler for registering user
+	@PostMapping("/do_register")
+	public String registerUser(@Valid @ModelAttribute("user") User user,BindingResult bResult,
+			@RequestParam(value = "agreement", defaultValue = "false") boolean agreement, Model model,
+			HttpSession session) {
+
+		try {
+			if (!agreement) {
+				System.out.println("user not aggred with the term and conditions");
+				throw new Exception("user not aggred with the term and conditions");
+			}
+			
+			if(bResult.hasErrors()) {
+				System.out.println("ERROR");
+				model.addAttribute("user",user);
+				
+				return "signup";
+			}
+
+			user.setRole("ROLE_USER");
+			user.setEnable(true);
+			user.setImage("deafult.png");
+			User result = this.userRepository.save(user);
+
+			System.out.println("Agreement: " + agreement);
+			System.out.println("User: " + result);
+			model.addAttribute("user", new User());
+			session.setAttribute("message", new Message("Succesfully Registered", "alert-success"));
+			return "signup";
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			model.addAttribute("user", user);
+			session.setAttribute("message", new Message("Something went Wrong!!" + e.getMessage(), "alert-danger"));
+			return "signup";
+		}
+
+	}
+}
+
+//	
+//	@GetMapping("/h")
+//	@ResponseBody
+//	public String test() {
+//		
+//		User user = new User();
+//		user.setName("FAS Cha");
+//		user.setEmail("FAS@nasdn.io");
+//		userRepository.save(user);
+//		System.out.print(userRepository);
+//		return "Working";
+//	}
+//}
---
+++ Smart_Contact_Manager/src/main/java/com/smart/entities/User.java
@@ -0,0 +1,131 @@
+package com.smart.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.AssertTrue;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+
+
+@Entity
+@Table(name="USER")
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	
+	@NotBlank(message = "user Name can not be empty")
+	@Size(min= 2, max=20, message="min 2 and max 20 chracters are allowd!!!")
+	private String name;
+	
+	@Column(unique = true)
+	@Email(regexp="^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$")
+	private String email;
+	
+	@NotBlank(message = "Can not be empty")
+	private String passward;
+	
+	
+	private String role;
+	
+//	@AssertTrue(message="must agreed term and condition")
+	private boolean enable;
+	private String image;
+	
+	@Column(length =500)
+	@NotBlank(message = "Can not be empty")
+	private String about;
+	
+	@OneToMany(cascade = CascadeType.ALL)
+	private List<Contact> contacts = new ArrayList();
+	
+	public User() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public User(int id, String name, String email, String passward, String role, boolean enable, String image,
+			String about) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.passward = passward;
+		this.role = role;
+		this.enable = enable;
+		this.image = image;
+		this.about = about;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassward() {
+		return passward;
+	}
+	public void setPassward(String passward) {
+		this.passward = passward;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	public boolean isEnable() {
+		return enable;
+	}
+	public void setEnable(boolean enable) {
+		this.enable = enable;
+	}
+	public String getImage() {
+		return image;
+	}
+	public void setImage(String image) {
+		this.image = image;
+	}
+	public String getAbout() {
+		return about;
+	}
+	public void setAbout(String about) {
+		this.about = about;
+	}
+	public List<Contact> getContacts() {
+		return contacts;
+	}
+	public void setContacts(List<Contact> contacts) {
+		this.contacts = contacts;
+	}
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + ", email=" + email + ", passward=" + passward + ", role=" + role
+				+ ", enable=" + enable + ", image=" + image + ", about=" + about + ", contacts=" + contacts + "]";
+	}
+	
+	
+}
