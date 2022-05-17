915feec60204a866c4466fe3418aa86b20ab00aa
nchafle1575 <nchafle@gmail.com>
2022-05-17 12:44:51 +0530

    some more changes are still pending and update button pending

--- src/main/java/com/smart/controller/UserController.java
+++ src/main/java/com/smart/controller/UserController.java
@@ -1,17 +1,36 @@
-package com.smart.controller;
+  package com.smart.controller;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.security.Principal;
+import java.util.List;
+import java.util.Optional;
+
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.smart.dao.ContactRepository;
 import com.smart.dao.UserRepository;
 import com.smart.entities.Contact;
 import com.smart.entities.User;
+import com.smart.helper.Message;
 
 @Controller
 @RequestMapping("/user")
@@ -19,8 +38,11 @@ public class UserController {
 
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired
+	private ContactRepository contactRepository;
 
-	//method for adding common data to response
+	// method for adding common data to response
 	@ModelAttribute
 	public void addCommonData(Model model, Principal principal) {
 		String userName = principal.getName();
@@ -33,14 +55,13 @@ public class UserController {
 		model.addAttribute("user", user);
 	}
 
-	//dashboard home
+	// dashboard home
 	@RequestMapping("/index")
 	public String dashboard(Model model, Principal principal) {
 
 		return "normal/user_dashboard";
 	}
-	
-	
+
 	// open form add handler
 	@GetMapping("/add-contact")
 	public String openAddContactForm(Model model, Principal principal) {
@@ -48,4 +69,105 @@ public class UserController {
 		model.addAttribute("contact", new Contact());
 		return "normal/add_contact_form";
 	}
+
+	// process add contact form
+	@PostMapping("/process-contact")
+	public String processContact(@ModelAttribute Contact contact, @RequestParam("profileImage") MultipartFile file,
+			Principal principal, HttpSession session) {
+
+		try {
+			String name = principal.getName();
+			User user = this.userRepository.getUserByUserName(name);
+
+			// processing and uploading file.....
+			if (file.isEmpty()) {
+				System.out.println("file is empty!!!!!!!!!!!");
+//				contact.setImage("contact.png");
+
+			} else {
+				System.out.println("inside else blok");
+				contact.setImage(file.getOriginalFilename());
+				File savefile = new ClassPathResource("static/images").getFile();
+				Path path = Paths.get(savefile.getAbsolutePath() + File.separator + file.getOriginalFilename());
+
+				Files.copy(file.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
+				System.out.println("images is uploaded");
+			}
+			contact.setUser(user);
+			user.getContacts().add(contact);
+			this.userRepository.save(user);
+
+			System.out.println("DATA" + contact.toString());
+			System.out.println("Added to database");
+
+			// message success...
+			session.setAttribute("message", new Message("Your COntact is added", "success"));
+
+		} catch (Exception e) {
+			System.out.println("ERROR" + e.getMessage());
+			e.getStackTrace();
+
+			// error message
+			session.setAttribute("message", new Message("Something went Wrong!! try again", "danger"));
+		}
+		return "normal/add_contact_form";
+	}
+
+	// show contact handler
+	//per page 5
+	//current page
+	
+	@GetMapping("/show-contact/{page}")
+	public String showContacts(@PathVariable("page") Integer page ,Model m, Principal principal) {
+		m.addAttribute("title", "Show User Contact");
+
+		// contact ki list ko bhejna hy
+		
+		Pageable pageable = PageRequest.of(page, 5);
+	
+		String userName = principal.getName();
+		User user = this.userRepository.getUserByUserName(userName);
+		Page<Contact> contacts =  this.contactRepository.findContactByUser(user.getId(), pageable);
+			
+		m.addAttribute("contacts", contacts);
+		m.addAttribute("currentPage", page);
+		m.addAttribute("totalPages", contacts.getTotalPages());
+		
+		return "normal/show_contact";
+	}
+	
+	//showing particular contact details
+	@RequestMapping("/{cid}/contact")
+	public String showContactDetails(@PathVariable("cid") Integer cid, Model m, Principal principal) {
+		System.out.println("CID"+ cid);
+		
+		
+		Optional<Contact> contactOpt= this.contactRepository.findById(cid);
+		Contact contacts = contactOpt.get();
+		
+		String userName = principal.getName();
+		User user = this.userRepository.getUserByUserName(userName);
+		
+		if(user.getId() == contacts.getUser().getId())
+			m.addAttribute("contacts", contacts);
+		
+		
+		return "normal/contact_details";
+	}
+	
+	@GetMapping("delete/{cid}")
+	public String deleteContact(@PathVariable("cid") Integer cid, Model m, HttpSession session) {
+		
+		
+		Optional<Contact> contactOpational = this.contactRepository.findById(cid);
+		Contact contact = contactOpational.get();
+		
+		contact.setUser(null);
+		this.contactRepository.delete(contact);
+		
+		session.setAttribute("message", new Message("Contact deleted succesfully...", "success"));
+		return "redirect:/user/show-contact/0";
+		
+	}
+	
 }
--- src/main/java/com/smart/entities/Contact.java
+++ src/main/java/com/smart/entities/Contact.java
@@ -7,97 +7,129 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 
 @Entity
-@Table(name="CONTACT")
+@Table(name = "CONTACT")
 public class Contact {
- 
- @Id
- @GeneratedValue(strategy = GenerationType.AUTO)
- private int cid;
- private String name;
- private String nickName;
- private String work;
- private String email;
- private String image;
- private String phone;
- @Column(length=5000)
- private String description;
- 
- @ManyToOne
- private User user;
- 
-public Contact(int cid, String name, String nickName, String work, String email, String image, String phone,
-		String description) {
-	super();
-	this.cid = cid;
-	this.name = name;
-	this.nickName = nickName;
-	this.work = work;
-	this.email = email;
-	this.image = image;
-	this.phone = phone;
-	this.description = description;
-}
-public Contact() {
-	super();
-	// TODO Auto-generated constructor stub
-}
-public int getCid() {
-	return cid;
-}
-public void setCid(int cid) {
-	this.cid = cid;
-}
-public String getName() {
-	return name;
-}
-public void setName(String name) {
-	this.name = name;
-}
-public String getNickName() {
-	return nickName;
-}
-public void setNickName(String nickName) {
-	this.nickName = nickName;
-}
-public String getWork() {
-	return work;
-}
-public void setWork(String work) {
-	this.work = work;
-}
-public String getEmail() {
-	return email;
-}
-public void setEmail(String email) {
-	this.email = email;
-}
-public String getImage() {
-	return image;
-}
-public void setImage(String image) {
-	this.image = image;
-}
-public String getPhone() {
-	return phone;
-}
-public void setPhone(String phone) {
-	this.phone = phone;
-}
-public String getDescription() {
-	return description;
-}
-public void setDescription(String description) {
-	this.description = description;
-}
-public User getUser() {
-	return user;
-}
-public void setUser(User user) {
-	this.user = user;
-}
- 
- 
- 
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int cid;
+	
+	@NotBlank(message = "Name can not be empty")
+	private String name;
+	private String secondName;
+	private String work;
+
+	@Column(unique = true)
+	@Email(regexp = "^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$")
+	private String email;
+	
+	private String image;
+	
+	private String phone;
+	
+	@Column(length = 5000)
+	private String description;
+
+	@ManyToOne
+	private User user;
+
+	public Contact(int cid, String name, String secondName, String work, String email, String image, String phone,
+			String description) {
+		super();
+		this.cid = cid;
+		this.name = name;
+		this.secondName = secondName;
+		this.work = work;
+		this.email = email;
+		this.image = image;
+		this.phone = phone;
+		this.description = description;
+	}
+
+	public Contact() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getCid() {
+		return cid;
+	}
+
+	public void setCid(int cid) {
+		this.cid = cid;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSecondName() {
+		return secondName;
+	}
+
+	public void setSecondName(String secondName) {
+		this.secondName = secondName;
+	}
+
+	public String getWork() {
+		return work;
+	}
+
+	public void setWork(String work) {
+		this.work = work;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+//@Override
+//public String toString() {
+//	return "Contact [cid=" + cid + ", name=" + name + ", nickName=" + nickName + ", work=" + work + ", email=" + email
+//			+ ", image=" + image + ", phone=" + phone + ", description=" + description + ", user=" + user + "]";
+//}
+
 }
